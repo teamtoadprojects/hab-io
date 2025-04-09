9c086f38346f49ccc320d557f73d00ec2b279fa0
Tom Wardill <tom@howrandom.net>
2025-04-09 12:40:18 +0100

    Add SSDV decoding

--- output/ssdv_to_disk.py
+++ output/ssdv_to_disk.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import subprocess
 
 from core import PluginBase, Payload, PayloadType
 
@@ -12,11 +13,18 @@ class ssdv_to_disk(PluginBase):
     async def output(self, payload: Payload):
         self.logger.info("Outputting payload", payload=payload)
 
-        filename = f"{payload.callsign}_{payload.payload_id}.ssdv"
-        full_output_path = Path(self.config["directory"]) / filename
+        root_name = f"{payload.callsign}_{payload.payload_id}"
+        filename = f"{root_name}.ssdv"
+        directory = Path(self.config["directory"])
+        full_output_path = directory / filename
         with open(full_output_path, 'ab') as output_file:
             data = bytes.fromhex(payload.other_fields["ssdv"])
             output_file.write(data)
             output_file.flush()
+
+        subprocess.call(
+            f"/ssdv/ssdv -d {directory / filename} {directory / root_name}.jpg",
+            shell=True,
+        )
 
         self.logger.info("Wrote SSDV packet to disk", filename=filename)
