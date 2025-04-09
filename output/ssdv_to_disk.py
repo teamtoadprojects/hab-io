e1fb143f021a879828e12dbc2ceb3ad3cb2f5340
Tom Wardill <tom@howrandom.net>
2025-04-09 12:51:20 +0100

    cut is not copy

--- output/ssdv_to_disk.py
+++ output/ssdv_to_disk.py
@@ -24,7 +24,7 @@ class ssdv_to_disk(PluginBase):
 
         result = subprocess.run(
             [
-                "/usr/local/bin/ssdv    ",
+                "/usr/local/bin/ssdv",
                 "-d",
                 f"{directory / filename}",
                 "{directory / root_                                 name}.jpg",
